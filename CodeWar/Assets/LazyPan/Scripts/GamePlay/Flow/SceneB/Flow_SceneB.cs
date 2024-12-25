6f4796cc4a447fb83febe1a589894d1a15b661b7
Evoreek <wh_961230@163.com>
2024-12-25 23:07:13 +0800

    场景玩家初步添加

--- CodeWar/Assets/LazyPan/Scripts/GamePlay/Flow/SceneB/Flow_SceneB.cs
+++ CodeWar/Assets/LazyPan/Scripts/GamePlay/Flow/SceneB/Flow_SceneB.cs
@@ -4,6 +4,7 @@ namespace LazyPan {
     public class Flow_SceneB : Flow {
 		private Comp UI_SceneB;
 
+		private Entity Obj_Terrain_TerrainB;
 		private Entity Obj_Camera_Camera;
 		private Entity Obj_Player_Player;
 		private Entity Obj_Event_SceneBUI;
@@ -13,6 +14,7 @@ namespace LazyPan {
             ConsoleEx.Instance.ContentSave("flow", "Flow_SceneB  战斗场景B流程");
 			UI_SceneB = UI.Instance.Open("UI_SceneB");
 
+			Obj_Terrain_TerrainB = Obj.Instance.LoadEntity("Obj_Terrain_TerrainB");
 			Obj_Camera_Camera = Obj.Instance.LoadEntity("Obj_Camera_Camera");
 			Obj_Player_Player = Obj.Instance.LoadEntity("Obj_Player_Player");
 			Obj_Event_SceneBUI = Obj.Instance.LoadEntity("Obj_Event_SceneBUI");
@@ -36,6 +38,7 @@ namespace LazyPan {
 			Obj.Instance.UnLoadEntity(Obj_Event_SceneBUI);
 			Obj.Instance.UnLoadEntity(Obj_Player_Player);
 			Obj.Instance.UnLoadEntity(Obj_Camera_Camera);
+			Obj.Instance.UnLoadEntity(Obj_Terrain_TerrainB);
 
 			UI.Instance.Close("UI_SceneB");
 
